b9b67c3a46dda6cf5830a83da994cbc35bd631e1
ChrisMugwagwa <mugwagwachristian@gmail.com>
2019-06-26 22:53:13 +0100

    Upload

---
+++ Filter/src/ofApp.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxMaxim.h"
+#include "fft.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		void audioOut(float * buffer, int bufferSize, int nChannels);
+
+		maxiOsc osc1, osc2;
+
+		double frequency, currentSample;
+		unsigned int bufferSize, sampleRate;
+
+		vector <float> lAudio;
+		vector <float> rAudio;
+
+		maxiFFT myfft;
+		int spectleft;
+		int spectbottom;
+		int fftSize;
+		int counter;
+		int fttHeight;
+		int fftWidth;
+		
+};
---
+++ Sequencer/src/ofApp.cpp
@@ -0,0 +1,171 @@
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	/////////////Basic Setup////////////////////////
+	ofBackground(34);
+	sampleRate = 44100;
+	bufferSize = 512;
+	frequency = 300;
+	ofSoundStreamSetup(2, 0, this, sampleRate, bufferSize, 4);
+	lAudio.assign(bufferSize, 0.0);
+	rAudio.assign(bufferSize, 0.0);
+	fftSize = 1024;
+	myfft.setup(fftSize, 512, 256);
+	spectbottom = 800;
+	spectleft = 10;
+	counter = 0;
+	fttHeight = 1;
+	fftWidth = 2;
+	////////////////////Basic Setup////////////////
+
+	kick.load("kick.wav",0);	
+	snare.load("snare.wav",0);
+	hihat.load("hihat.wav",0);
+	shaker.load("shaker.wav",0);
+	
+
+}
+//--------------------------------------------------------------
+void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
+	for (int i = 0; i < bufferSize; i++) {
+		currentSample = osc1.sinewave(frequency) + osc2.saw(frequency * 0.6);
+
+		if (myfft.process(currentSample)) {
+			myfft.magsToDB();
+		}
+
+		lAudio[i] = output[i * nChannels] = currentSample;
+		rAudio[i] = output[i * nChannels + 1] = currentSample;
+
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	
+	///////DRAW THE CHANNELS////////////////
+	ofSetColor(225);
+	ofNoFill();
+
+	// draw the left channel:
+	ofPushStyle();
+	ofPushMatrix();
+	ofTranslate(32, 40, 0);
+
+	ofSetColor(225);
+	ofDrawBitmapString("Left Channel", 4, 18);
+
+	ofSetLineWidth(1);
+	ofRect(0, 0, 900, 200);
+
+	ofSetColor(245, 58, 135);
+	ofSetLineWidth(3);
+
+	ofBeginShape();
+	for (int i = 0; i < lAudio.size(); i++) {
+		float x = ofMap(i, 0, lAudio.size(), 0, 900, true);
+		ofVertex(x, 100 - (lAudio[i] * 40.0f));
+	}
+	ofEndShape(false);
+
+	ofPopMatrix();
+	ofPopStyle();
+
+	// draw the right channel:
+	ofPushStyle();
+	ofPushMatrix();
+	ofTranslate(32, 250, 0);
+
+	ofSetColor(225);
+	ofDrawBitmapString("Right Channel", 4, 18);
+
+	ofSetLineWidth(1);
+	ofRect(0, 0, 900, 200);
+
+	ofSetColor(245, 58, 135);
+	ofSetLineWidth(3);
+
+	ofBeginShape();
+	for (int i = 0; i < rAudio.size(); i++) {
+		float x = ofMap(i, 0, rAudio.size(), 0, 900, true);
+		ofVertex(x, 100 - (rAudio[i] * 40.0f));
+	}
+	ofEndShape(false);
+
+	ofPopMatrix();
+	ofPopStyle();
+	///////////end drawing the channels/////////////////////////////
+
+	//////////// draw the fft////////////////////////////
+	for (int i = 0; i < fftSize / 2; i++) {
+		
+		ofDrawLine((spectleft + i)*fftWidth, spectbottom, (spectleft + i)*fftWidth, spectbottom -(myfft.magnitudes[i] * fttHeight));
+		
+		// context.stroke();
+		
+	}
+	///////////end drawing the fft//////////////////////////
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+	frequency = ofMap(x, 0, ofGetWidth(), 20, 1000);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
